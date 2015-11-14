54f40042928d6b8f9353fa40dcbab9c1695b789e
Leo Febey <overclockedamiga@gmail.com>
2015-11-14 16:59:28 +1100

    Basic menu system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 var reload      = browserSync.reload;
+var path        = require('path');
 
-gulp.task('browser-sync', function(){
+gulp.task('browser-sync', ['sass'], function(){
   browserSync.init({
       server: {
           baseDir: "./",
           port: 8000
       }
   });
-  //gulp.watch(path.join("js", "*.js"), ['browser-sync']);
+  gulp.watch(path.join("sass", "*.scss"), ['sass']);
   gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function(){
+  gulp.src(path.join("sass", "*.scss"))
+  .pipe(sass.sync().on('error', sass.logError))
+  .pipe(gulp.dest('./css'));
+  browserSync.reload();
 });
 
 gulp.task('connect', function() {
     connect.server();
 });
+
+//TODO: gulp-publish to make builds..
 
 gulp.task('default', ['browser-sync']);
